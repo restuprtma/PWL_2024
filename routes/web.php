511d101e917d1613fa0338f1a2b909f16192659a
restuprtma <restu.prtma05@gmail.com>
2025-02-18 10:27:05 +0700

    Jobsheet 2 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,69 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return 'Hello, World!';
+});
+
+Route::get('/world', function(){
+    return 'World';
+});
+
+Route::get('/welcome', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return 'Fais Restu pratama - 2341720076';
+});
+
+Route::get('/user/{name}', function($name){
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Post ke-' . $postId . ' Komentar ke-' . $commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function(){
+    //
+})->name('profile');
+
+Route::get(
+    '/user/profile',
+    [UserProfileController::class, 'show']
+)->name('profile');
+
+// Generating URLs...
+$url = route('profile');
+
+// Generating Redirects...
+return redirect()->route('profile');
+
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+});
+Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
