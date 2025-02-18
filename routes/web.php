2fa41a3ca1feeb5cc9589fe9818596d11ad30e56
restuprtma <restu.prtma05@gmail.com>
2025-02-18 10:32:20 +0700

    Jobsheet 2 -Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -48,37 +48,3 @@ Route::get('/articles/{id}', function($id){
 Route::get('/user/{name?}', function($name='John'){
     return 'Nama saya '.$name;
 });
-
-Route::get('/user/profile', function(){
-    //
-})->name('profile');
-
-Route::get(
-    '/user/profile',
-    [UserProfileController::class, 'show']
-)->name('profile');
-
-// Generating URLs...
-$url = route('profile');
-
-// Generating Redirects...
-return redirect()->route('profile');
-
-Route::middleware(['first', 'second'])->group(function () {
-    Route::get('/', function () {
-        // Uses first & second middleware...
-});
-Route::get('/user/profile', function () {
-        // Uses first & second middleware...
-    });
-});
-Route::domain('{account}.example.com')->group(function () {
-    Route::get('user/{id}', function ($account, $id) {
-        //
-    });
-});
-Route::middleware('auth')->group(function () {
-    Route::get('/user', [UserController::class, 'index']);
-    Route::get('/post', [PostController::class, 'index']);
-    Route::get('/event', [EventController::class, 'index']);
-});
